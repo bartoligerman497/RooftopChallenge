a8e208a9fbc4533ff1c7b60307ca32bb6683fec2
German Bartoli <bartoligerman497@gmail.com>
2022-05-04 16:44:46 -0300

    Creación Matriz

--- RooftopChallenge/RooftopChallenge/Program.cs
+++ RooftopChallenge/RooftopChallenge/Program.cs
@@ -2,11 +2,21 @@
 
 namespace RooftopChallenge
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Rooftopper");
+
+            int cantidadFilas = Convert.ToInt32(Console.ReadLine());
+
+            int cantidadColumnas = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matriz;
+            matriz = new int[cantidadFilas, cantidadColumnas];
+
+            Console.WriteLine("Cantidad de filas de la matriz: " + matriz.GetLength(0));
+            Console.WriteLine("Cantidad de columnas de la matriz: " + matriz.GetLength(1));
         }
     }
 }
